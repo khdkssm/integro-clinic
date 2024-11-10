9840ed4eeffc1cb545748dafb390b6d6bedcaaf2
MaulikKayastha <80619313+MaulikKayastha@users.noreply.github.com>
2024-11-10 18:06:17 +0000

    file structure revision

---
+++ src/test/java/com/integro/IntegroClinicClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegroClinicClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
